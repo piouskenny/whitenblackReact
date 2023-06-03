b05f272142024cc0cc0a7f4bc1abfc1b1e9a950a
piouskenny <kehindeu13@gmail.com>
2023-06-03 01:35:14 +0100

    Done

--- src/components/content/content.jsx
+++ src/components/content/content.jsx
@@ -39,7 +39,7 @@ const Content = ({ searchContent }) => {
   return (
     <div className="mt-10 md:mt-18">
       <div className="">
-        <div className="flex">
+        <div className="flex items-center justify-between">
           <div>
             {/* Search Word */}
             <h1 className="text-gray-800 dark:text-white font-bold text-4xl md:text-5xl lg:text-6xl">
@@ -49,6 +49,24 @@ const Content = ({ searchContent }) => {
             <p className="text-purple-600 pt-2 md:pt-4 italic font-light md:text-xl">
               {apiResponse[0].phonetic}
             </p>
+          </div>
+
+          <div className="text-purple-800 bg-purple-300 p-2 rounded-full">
+            {/* <audio src="" controls/> */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M19.114 5.636a9 9 0 010 12.728M16.463 8.288a5.25 5.25 0 010 7.424M6.75 8.25l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z"
+              />
+            </svg>
           </div>
         </div>
 
